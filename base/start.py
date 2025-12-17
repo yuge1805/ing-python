a710020b4f46c3190fbbabe83631e209d382cef8
Wen <yuge1805@yeah.net>
2025-12-17 20:01:37 +0800

    feat: start

---
+++ base/start.py
@@ -0,0 +1,31 @@
+print("hello world")
+print("hello world2")
+print("hello world3");print("hello world3")
+
+# 数据类型
+# 整数（int）
+print(100)
+# 浮点数/小数 （float）
+print(3.14)
+# 布尔值（bool）
+print(True)
+print(False)
+# 字符串（str）
+print("Hello")
+# 空值（NoneType）
+print(None)
+
+# 变量
+# Python是动态类型语音，在程序运行时才进行类型检查，变量的类型可以在程序运行过程中改变（一个变量可以接受不同类型的值）
+# 不推荐
+n = True
+print(n)
+n = 3.22
+print(n)
+
+# 常用函数
+# type
+print(type(n))
+# isinstance
+print(isinstance(n, float))
+
